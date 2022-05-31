ba476e876d6c09c42c8501cabd94d2742a409c4e
/ɪτs-ʜʏρєʀ <106336633+ItsmeHyper13@users.noreply.github.com>
2022-05-31 11:15:26 +0530

    Add files via upload

---
+++ DevuMusic/plugins/tools/languages.py
@@ -0,0 +1,111 @@
+# Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+# Found on github < https://github.com/ItsmeHyper13 >.
+
+from pykeyboard import InlineKeyboard
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, Message
+
+from config import BANNED_USERS
+from strings import get_command, get_string
+from DevuMusic import app
+from DevuMusic.utils.database import get_lang, set_lang
+from DevuMusic.utils.decorators import (ActualAdminCB, language,
+                                         languageCB)
+
+# Languages Available
+
+
+def lanuages_keyboard(_):
+    keyboard = InlineKeyboard(row_width=2)
+    keyboard.row(
+        InlineKeyboardButton(
+            text="🏴󠁧󠁢󠁥󠁮󠁧󠁿 English", callback_data="languages:en"
+        ),
+        InlineKeyboardButton(text="🇮🇳 हिन्दी", callback_data="languages:hi"),
+    )
+
+    keyboard.row(
+        InlineKeyboardButton(text="🇱🇰 සිංහල", callback_data="languages:si"),
+        InlineKeyboardButton(
+            text="🇦🇿 Azərbaycan", callback_data="languages:az"
+        ),
+    )
+
+    keyboard.row(
+        InlineKeyboardButton(text="🇮🇳 ગુજરાતી", callback_data="languages:gu"),
+        InlineKeyboardButton(
+            text="🇹🇷 Türkiye Türkçesi", callback_data="languages:tr"
+        ),
+    )
+
+    keyboard.row(
+        InlineKeyboardButton(text="🐶 Cheems", callback_data="languages:cheems")
+    )
+
+    keyboard.row(
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"], callback_data="settingsback_helper"
+        ),
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+    )
+
+    return keyboard
+
+
+LANGUAGE_COMMAND = get_command("LANGUAGE_COMMAND")
+
+
+@app.on_message(
+    filters.command(LANGUAGE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@language
+async def langs_command(client, message: Message, _):
+    keyboard = lanuages_keyboard(_)
+    await message.reply_text(
+        _["setting_1"].format(message.chat.title, message.chat.id),
+        reply_markup=keyboard,
+    )
+
+
+@app.on_callback_query(filters.regex("LG") & ~BANNED_USERS)
+@languageCB
+async def lanuagecb(client, CallbackQuery, _):
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    keyboard = lanuages_keyboard(_)
+    return await CallbackQuery.edit_message_reply_markup(
+        reply_markup=keyboard
+    )
+
+
+@app.on_callback_query(
+    filters.regex(r"languages:(.*?)") & ~BANNED_USERS
+)
+@ActualAdminCB
+async def language_markup(client, CallbackQuery, _):
+    langauge = (CallbackQuery.data).split(":")[1]
+    old = await get_lang(CallbackQuery.message.chat.id)
+    if str(old) == str(langauge):
+        return await CallbackQuery.answer(
+            "You're already on same language", show_alert=True
+        )
+    try:
+        _ = get_string(langauge)
+        await CallbackQuery.answer(
+            "Successfully changed your language.", show_alert=True
+        )
+    except:
+        return await CallbackQuery.answer(
+            "Failed to change language or Language under update.",
+            show_alert=True,
+        )
+    await set_lang(CallbackQuery.message.chat.id, langauge)
+    keyboard = lanuages_keyboard(_)
+    return await CallbackQuery.edit_message_reply_markup(
+        reply_markup=keyboard
+    )
---
+++ DevuMusic/plugins/tools/ping.py
@@ -0,0 +1,40 @@
+# Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+# Found on github < https://github.com/ItsmeHyper13 >.
+
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from DevuMusic import app
+from DevuMusic.core.call import Devu
+from DevuMusic.utils import bot_sys_stats
+from DevuMusic.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(
+    filters.command(PING_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await Devu.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
+        )
+    )
