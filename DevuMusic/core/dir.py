f11ad51ea1780f4f28189a18bccbf384e34c4736
/ɪτs-ʜʏρєʀ <106336633+ItsmeHyper13@users.noreply.github.com>
2022-05-31 11:00:20 +0530

    Add files via upload

---
+++ DevuMusic/core/dir.py
@@ -0,0 +1,28 @@
+# Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+# Found on github < https://github.com/ItsmeHyper13 > .
+
+import os
+import sys
+from os import listdir, mkdir
+
+from ..logging import LOGGER
+
+
+def dirr():
+    if "assets" not in listdir():
+        LOGGER(__name__).warning(
+            "Assets Folder not Found. Please clone repository again."
+        )
+
+        sys.exit()
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".jpeg"):
+            os.remove(file)
+    if "downloads" not in listdir():
+        mkdir("downloads")
+    if "cache" not in listdir():
+        mkdir("cache")
+    LOGGER(__name__).info("Directories Updated.")
