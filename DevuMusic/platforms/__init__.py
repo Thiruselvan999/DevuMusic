2a48b92a623581ab8096c4f56e6a144b68636aba
/ɪτs-ʜʏρєʀ <106336633+ItsmeHyper13@users.noreply.github.com>
2022-05-31 11:01:25 +0530

    Add files via upload

---
+++ DevuMusic/platforms/__init__.py
@@ -0,0 +1,10 @@
+# Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+# Found on github < https://github.com/ItsmeHyper13 >
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
