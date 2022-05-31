708f11cb800ec9ebb1c88845d219af18efc2324a
/ɪτs-ʜʏρєʀ <106336633+ItsmeHyper13@users.noreply.github.com>
2022-05-31 11:10:46 +0530

    Add files via upload

---
+++ DevuMusic/plugins/bot/inline.py
@@ -0,0 +1,77 @@
+# Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+# Found on github < https://github.com/ItsmeHyper13 >.
+
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup,
+                            InlineQueryResultPhoto)
+from youtubesearchpython.__future__ import VideosSearch
+
+from config import BANNED_USERS, MUSIC_BOT_NAME
+from DevuMusic import app
+from DevuMusic.utils.inlinequery import answer
+
+
+@app.on_inline_query(~BANNED_USERS)
+async def inline_query_handler(client, query):
+    text = query.query.strip().lower()
+    if text.strip() == "":
+        try:
+            await client.answer_inline_query(
+                query.id, results=answer, cache_time=10
+            )
+        except:
+            return
+    else:
+        a = VideosSearch(text, limit=20)
+        result = (await a.next()).get("result")
+        answers = []
+        for x in range(15):
+            title = (result[x]["title"]).title()
+            duration = result[x]["duration"]
+            views = result[x]["viewCount"]["short"]
+            thumbnail = result[x]["thumbnails"][0]["url"].split("?")[
+                0
+            ]
+            channellink = result[x]["channel"]["link"]
+            channel = result[x]["channel"]["name"]
+            link = result[x]["link"]
+            published = result[x]["publishedTime"]
+            description = f"{views} | {duration} Mins | {channel}  | {published}"
+            buttons = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="🎥 Watch on Youtube",
+                            url=link,
+                        )
+                    ],
+                ]
+            )
+            searched_text = f"""
+❇️**Title:** [{title}]({link})
+
+⏳**Duration:** {duration} Mins
+👀**Views:** `{views}`
+⏰**Published Time:** {published}
+🎥**Channel Name:** {channel}
+📎**Channel Link:** [Visit From Here]({channellink})
+
+__Reply with /play on this searched message to stream it on voice chat.__
+
+⚡️ ** Inline Search By {MUSIC_BOT_NAME} **"""
+            answers.append(
+                InlineQueryResultPhoto(
+                    photo_url=thumbnail,
+                    title=title,
+                    thumb_url=thumbnail,
+                    description=description,
+                    caption=searched_text,
+                    reply_markup=buttons,
+                )
+            )
+        try:
+            return await client.answer_inline_query(
+                query.id, results=answers
+            )
+        except:
+            return
